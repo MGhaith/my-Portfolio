f8d08c2edb7df4dec300663d33d315f018cedce0
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-08-23 22:51:04 +0100

    test pr 3

--- infra/modules/iam_github_oidc/main.tf
+++ infra/modules/iam_github_oidc/main.tf
@@ -27,14 +27,14 @@ data "aws_iam_policy_document" "assume_role" {
 
     condition {
       test     = "StringEquals"
-      variable = "token.actions.githubusercontent.com:sud"
+      variable = "token.actions.githubusercontent.com:aud"
       values   = ["sts.amazonaws.com"]
     }
 
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_owner}/${var.github_repo}:*"]
+      values   = ["repo:${var.github_owner}/${var.github_repo}:ref:${var.allowed_ref}"]
     }
   }
 }

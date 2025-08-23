966b61074c19934c766913c09de612701236fb81
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-08-23 19:13:41 +0100

    initial Vite React app setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "5rem" }}>
+      <h1>👋 Hi, I’m Ghaith</h1>
+      <p>Welcome to my cloud-native portfolio!</p>
+    </div>
+  );
+}
+
+export default App;

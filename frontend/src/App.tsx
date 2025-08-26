ef579768e7930b8d98e9c0362ba3846c53ecede7
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-08-26 17:36:22 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,29 @@
-import ProjectsList from "./components/ProjectsList";
-import ContactForm from "./components/ContactForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
+import { ThemeProvider } from "@/components/theme-provider";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div>
-      <h1>My Portfolio</h1>
-      <ProjectsList />
-      <ContactForm />
-    </div>
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="flex-grow pt-16">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/resume" element={<Resume />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }

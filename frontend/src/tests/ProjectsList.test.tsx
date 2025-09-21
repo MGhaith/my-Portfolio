854b841c4b79845f8b406641ff0efb9b91357ec1
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-09-21 14:48:21 +0100

    Update ProjectsList.test.tsx

--- frontend/src/tests/ProjectsList.test.tsx
+++ frontend/src/tests/ProjectsList.test.tsx
@@ -4,7 +4,7 @@ import { MemoryRouter } from "react-router-dom";
 import ProjectsList from "../components/ProjectsList";
 import * as api from "../api/api";
 
-vi.spyOn(api, "getProjects").mockResolvedValue([{ id: "1", title: "Test", description: "Desc", content: "Content", link: "", repo: "github.com", technologies: ["tech1", "tech2"] }]);
+vi.spyOn(api, "getProjects").mockResolvedValue([{ id: "1", title: "Test", description: "Desc", content: "Content", link: "", repo: "github.com", technologies: ["tech1", "tech2"], featured: true }]);
 
 test("renders projects", async () => {
   render(
